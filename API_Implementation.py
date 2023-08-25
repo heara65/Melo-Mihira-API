e27a151130a3dd3fe9579c6484e0152a2275803b
heara65 <oshanasanthusha@gmail.com>
2023-08-25 16:13:56 +0530

    f comit

--- API_Implementation.py
+++ API_Implementation.py
@@ -6,9 +6,6 @@ from fastapi import *
 import os
 
 import ModelLoading
-# import activity
-# import emotionModel
-# import music_recommendation
 import uuid
 
 IMAGEDIR = "Images/"
@@ -26,13 +23,13 @@ def GenderMapper(gender_):
 
 @app.get('/')
 def hello_world():
-    return "Hello World"
+    return "Hello World" # to test
 
 
 @app.post("/demographicsImage")
 async def create_upload_file(file: UploadFile = File(...)):
     file.filename = f"{uuid.uuid4()}.jpg"
-    contents = await file.read()  # <-- Important!
+    contents = await file.read()  
 
     # example of how you can save the file
     with open(f"{IMAGEDIR}{file.filename}", "wb") as f:
@@ -40,44 +37,10 @@ async def create_upload_file(file: UploadFile = File(...)):
 
     filepath = IMAGEDIR + file.filename
     Result = ModelLoading.finalImageOutput(filepath)
-    # Recommendations = music_recommendation.recommendDemographics(Result[0], GenderMapper(Result[1]), Result[2])
+   
 
     return {"Gender": Result[1], "Age": Result[0], "Ethnicity": Result[2]
-            # , "Recommendations": Recommendations
+            
             }
 
 
-# @app.post("/emotionImage")
-# async def create_upload_file(file: UploadFile = File(...)):
-#     file.filename = f"{uuid.uuid4()}.jpg"
-#     contents = await file.read()  # <-- Important!
-
-#     # example of how you can save the file
-#     with open(f"{IMAGEDIR}{file.filename}", "wb") as f:
-#         f.write(contents)
-
-#     filepath = IMAGEDIR + file.filename
-#     Result = emotionModel.input_image_Emotion(filepath)
-#     Recommendations = music_recommendation.recommendEmotion(Result)
-
-#     return {"Emotion": Result, "Recommendations": Recommendations}
-
-
-# @app.post("/activityVideo")
-# async def create_upload_file(file: UploadFile = File(...)):
-#     file.filename = f"{uuid.uuid4()}.avi"
-#     contents = await file.read()  # <-- Important!
-
-#     # example of how you can save the file
-#     with open(f"{IMAGEDIR}{file.filename}", "wb") as f:
-#         f.write(contents)
-
-#     filepath = IMAGEDIR + file.filename
-#     Result = activity.ActivityMusicRecommendation(filepath)
-#     Recommendations = music_recommendation.recommendActivity(Result)
-
-#     return {"Activity": Result, "Recommendations": Recommendations}
-
-
-# if __name__ == "__main__":
-#     uvicorn.run(app, port=8081, host='192.168.8.101')
--- ModelLoading.py
+++ ModelLoading.py
@@ -8,52 +8,26 @@ import os
 from tensorflow import keras
 
 Savedmodel = keras.models.load_model('OverallNewModel1.h5')
-# Savedmodel = keras.models.load_model('OverallNewModel.h5')
-# Savedmodel = keras.models.load_model('C:/Users/User/Desktop/New Folder/2021-069-IT18154054/API/OverallNewModel1.h5')
 print("Model Loaded")
 
 genderdict = {'m': 0, 'f': 1}
 
-countrylist = ['United States', 'Germany', 'United Kingdom', 'Poland', 'Russian Federation', 'Brazil', 'Sweden',
-               'Spain', 'Finland', 'Netherlands', 'Italy', 'France', 'Canada', 'Australia', 'Turkey', 'Norway',
-               'Czech Republic', 'Ukraine', 'Japan', 'Belgium', 'Mexico', 'Argentina', 'Switzerland', 'Austria',
-               'Romania', 'Portugal', 'Bulgaria', 'Chile', 'Denmark', 'Colombia', 'Greece', 'Hungary', 'Latvia',
-               'Slovakia', 'Croatia', 'Serbia', 'Lithuania', 'Estonia', 'Ireland', 'New Zealand', 'Belarus', 'Israel',
-               'India', 'Venezuela', 'Indonesia', 'Singapore', 'Slovenia', 'Korea, Republic of', 'China',
-               'South Africa', 'Malaysia', 'Philippines', 'Peru', 'Thailand', 'Moldova', 'Costa Rica', 'Iceland',
-               'Taiwan', 'Paraguay', 'Bosnia and Herzegovina', 'Antarctica', 'Puerto Rico', 'Georgia', 'Macedonia',
-               'Uruguay', 'Honduras', 'Barbados', 'Kazakhstan', 'Andorra', 'Saudi Arabia',
-               'United States Minor Outlying Islands', 'Djibouti', 'Cocos (Keeling) Islands', 'Tunisia', 'Egypt',
-               'Bolivia', 'Panama', 'Brunei Darussalam', 'Iran, Islamic Republic of', 'Dominican Republic',
-               'El Salvador', 'Haiti', 'Ecuador', 'Guatemala', 'Morocco', 'Pakistan', 'Burkina Faso', 'Azerbaijan',
-               'Cambodia', 'Hong Kong', 'Viet Nam', 'United Arab Emirates', 'Jamaica', 'Faroe Islands', 'Somalia',
-               'Guinea-Bissau', 'Micronesia, Federated States of', 'Tuvalu', "Cote D'Ivoire", 'Libyan Arab Jamahiriya',
-               'Nicaragua', 'Kyrgyzstan', 'Malta', 'Bermuda', 'Luxembourg', 'Kuwait', 'Cyprus',
-               'Heard Island and Mcdonald Islands', 'Christmas Island', 'Cuba', 'Niue', 'Aruba', 'Vanuatu', 'Dominica',
-               'Holy See (Vatican City State)', 'Uzbekistan', 'Bhutan', 'Montenegro', 'Reunion', 'Fiji',
-               'Netherlands Antilles', 'Lebanon', 'Liechtenstein']
-countrydict = dict(zip(countrylist, [i for i in range(len(countrylist))]))
 
 ethnicityList = ['white', 'black', 'asian', 'indian', 'others']
 ethnicitydict = dict(zip(ethnicityList, [i for i in range(len(ethnicityList))]))
 
 
-# print(countrydict)
 
-# Define a mapper functions
-def mapr1(key):
-    """ Maps numbers to categories (gender)"""
-    return genderdict[key]
-
-
-def mapr2(key):
-    """ Maps numbers to categories (country)"""
-    return countrydict[key]
+# # Define a mapper functions
+# def mapr1(key):
+#     """ Maps numbers to categories (gender)"""
+#     return genderdict[key]
 
 
-def mapr3(key):
-    """ Maps numbers to categories (country)"""
-    return ethnicitydict[key]
+
+# def mapr3(key):
+#     """ Maps numbers to categories (country)"""
+#     return ethnicitydict[key]
 
 
 ID_GENDER_MAP = {0: 'male', 1: 'female'}
@@ -71,10 +45,8 @@ def GenderMapper(gender_):
 
 
 from skimage import io
-# from keras.preprocessing import image
 from PIL import Image
 from matplotlib import pyplot
-# from pprint import pprint
 import keras
 from keras.preprocessing import image
 
@@ -83,10 +55,8 @@ def finalImageOutput(path):
     imagess = []
     IMGpath = path
 
-    # img = image.load_img(IMGpath)
+ 
     img = Image.open(IMGpath)
-
-    # Emotion = input_image_Emotion(IMGpath)
 
     img = img.resize((198, 198))
     img = np.array(img) / 255.0
